561d6adaf383a23d3be3e55198d06c1a161d548f
INSPIRAFIRMA <216106791+lnspirafirmaGPK@users.noreply.github.com>
2025-12-06 05:49:26 +0700

    Create components/ai-elements/message.tsx

---
+++ components/ai-elements/message.tsx
@@ -0,0 +1,16 @@
+export const MessageContent = ({
+  children,
+  className,
+  ...props
+}: MessageContentProps) => (
+  <div
+    className={cn(
+      'flex flex-col gap-2 text-sm text-foreground',
+      'group-[.is-user]:bg-primary group-[.is-user]:text-primary-foreground group-[.is-user]:px-4 group-[.is-user]:py-3',
+      className,
+    )}
+    {...props}
+  >
+    <div className="is-user:dark">{children}</div>
+  </div>
+);
